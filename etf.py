a987b31ced1e228f2e8cbf574ec39a7add7aa328
william3333 <60163837+william3333@users.noreply.github.com>
2023-09-30 10:53:58 +0800

    Update etf.py

--- etf.py
+++ etf.py
@@ -10,7 +10,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from datetime import datetime
-import matplotlib.pyplot as plt
 from cvxopt import matrix, solvers
 
 # Set the Streamlit app title
